a25f0ea28d5b35e4998af3ba9a1fc79d638454de
Farhad <dev.farhadhossain@gmail.com>
2023-12-29 18:02:19 +0600

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <h1 className='text-center text-5xl font-bold'>Here instrall react router dom, daisyUI, TailwindCss</h1>
+    </div>
+  )
+}
+
+export default Home
